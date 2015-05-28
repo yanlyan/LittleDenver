932ae58cdcf11a8e80ee3fb7596d4448bed8b8b2
Lyan Dwi Pangestu <gloriancrack@gmail.com>
2015-05-28 11:23:46 +0700

    First Commit

---
+++ app/src/main/java/com/example/lyan/littledenver/SignUpActivity.java
@@ -0,0 +1,52 @@
+package com.example.lyan.littledenver;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class SignUpActivity extends ActionBarActivity {
+    private String array_spinner[];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        // Here come all the options that you wish to show depending on the
+        // size of the array.
+        array_spinner=new String[5];
+        array_spinner[0]="option 1";
+        array_spinner[1]="option 2";
+        array_spinner[2]="option 3";
+        array_spinner[3]="option 4";
+        array_spinner[4]="option 5";
+        Spinner s = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, array_spinner);
+        s.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sign_up, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
